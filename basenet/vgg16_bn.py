b4bf8377d3046a720b25d768e408095ea9dfdfc1
Al <camrik55@yahoo.com>
2024-04-30 13:50:14 -0400

    Update vgg16_bn.py

--- basenet/vgg16_bn.py
+++ basenet/vgg16_bn.py
@@ -4,22 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.init as init
 from torchvision import models
-#from torchvision.models.vgg import model_urls
-import pkgutil
-import torchvision
-from importlib import import_module
 
-def get_torchvision_models():
-    model_urls = dict()
-    for _, name, ispkg in pkgutil.walk_packages(torchvision.models.__path__):
-        if ispkg:
-            continue
-        _zoo = import_module(f'torchvision.models.{name}')
-        if hasattr(_zoo, 'model_urls'):
-            _urls = getattr(_zoo, 'model_urls')
-            model_urls.update(_urls)
-    return model_urls
-    
 def init_weights(modules):
     for m in modules:
         if isinstance(m, nn.Conv2d):
@@ -36,7 +21,7 @@ def init_weights(modules):
 class vgg16_bn(torch.nn.Module):
     def __init__(self, pretrained=True, freeze=True):
         super(vgg16_bn, self).__init__()
-        model_urls['vgg16_bn'] = model_urls['vgg16_bn'].replace('https://', 'http://')
+        # model_urls['vgg16_bn'] = model_urls['vgg16_bn'].replace('https://', 'http://')
         vgg_pretrained_features = models.vgg16_bn(pretrained=pretrained).features
         self.slice1 = torch.nn.Sequential()
         self.slice2 = torch.nn.Sequential()
